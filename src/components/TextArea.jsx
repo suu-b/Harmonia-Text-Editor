b392dc54eeaa4ca7bf4dbaa63f93a6419a5bb15d
ShubhamThakur025 <thakur0805@gmail.com>
2025-02-22 16:52:26 +0530

    feat: hotkeys

--- src/components/TextArea.jsx
+++ src/components/TextArea.jsx
@@ -1,14 +1,81 @@
 import React, { useRef } from "react"
 import { Editable } from "slate-react"
+import { Transforms, Editor, Node } from "slate"
+import * as Alignment from "../format/Alignment"
+import * as Inserts from "../format/Inserts"
+import * as Marks from "../format/Marks"
+import * as Lists from "../format/Lists"
 
-const TextArea = ({ renderElement, renderLeaf}) => {
+const TextArea = ({ editor, renderElement, renderLeaf }) => {
+
+  const handleKeys = (e) => {
+    const { selection } = editor
+
+    if (e.ctrlKey && e.altKey && e.key === "Backspace") {
+      if (selection && Editor.isStart(editor, selection.anchor, selection.anchor.path)) {
+        const [node, path] = Editor.node(editor, selection.anchor.path)
+
+        if (Editor.isBlock(editor, node) && Node.string(node) === "") {
+          e.preventDefault()
+
+          const parentBlock = Editor.above(editor, { match: (n) => Editor.isBlock(editor, n) })
+          const isLastBlock = Editor.isEnd(editor, selection.focus, path)
+
+          if (parentBlock && !isLastBlock) {
+            Transforms.removeNodes(editor)
+          } else {
+            Transforms.setNodes(editor, { type: "paragraph", children: [{ text: "" }] })
+          }
+        }
+      }
+    }
+    else if (e.ctrlKey) {
+      switch (e.key) {
+        case "b":
+          e.preventDefault()
+          Marks.toggleMark(editor, "bold")
+          break
+        case "i":
+          e.preventDefault()
+          Marks.toggleMark(editor, "italic")
+          break
+        case "u":
+          e.preventDefault()
+          Marks.toggleMark(editor, "underline")
+          break
+        case "`":
+          e.preventDefault()
+          Inserts.insertCodeBlock(editor)
+          break
+        default:
+          break
+      }
+    } else if (e.shiftKey) {
+      switch (e.key) {
+        case "Enter":
+          e.preventDefault()
+          Transforms.insertText(editor, "\n")
+          break
+        case "Tab":
+          e.preventDefault()
+          Lists.outdentList(editor)
+          break
+        default:
+          break
+      }
+    }
+  }
+
+
   return (
     <Editable
       renderElement={renderElement}
       renderLeaf={renderLeaf}
+      onKeyDown={handleKeys}
       className="px-24 py-12 pb-24 w-[794px] min-h-[1123px] m-auto bg-white shadow-lg border border-gray-200 rounded ring-0 focus:ring-0 shadow-none focus:shadow-none focus:outline-none"
     />
   )
+
 }
 
 export default TextArea
--- src/components/TextEditor.jsx
+++ src/components/TextEditor.jsx
@@ -3,9 +3,8 @@ import ToolBar from "./ToolBar"
 import TextArea from "./TextArea"
 import snoopyWrites from "../assets/snoopy_writes.jpg"
 import { Slate } from "slate-react"
-import { useDispatch, useSelector } from "react-redux"
+import { useDispatch } from "react-redux"
 import { setText } from "../store/editorslice"
-import { useState } from "react"
 
 const TextEditor = ({ editor, initialValue, renderElement, renderLeaf }) => {
   const dispatch = useDispatch()
@@ -26,7 +25,7 @@ const TextEditor = ({ editor, initialValue, renderElement, renderLeaf }) => {
       }}>
       <section id="text-editor" className="flex w-screen h-screen bg-gray-50 overflow-hidden">
         <section className="w-[80%] overflow-y-auto flex justify-center items-start p-12 bg-gray-50">
-          <TextArea renderElement={renderElement} renderLeaf={renderLeaf} />
+          <TextArea renderElement={renderElement} renderLeaf={renderLeaf} editor={editor} />
         </section>
 
         <section
@@ -34,7 +33,7 @@ const TextEditor = ({ editor, initialValue, renderElement, renderLeaf }) => {
           className="h-screen w-[20vw] fixed right-0 top-0 flex flex-col items-start justify-start bg-white shadow-lg"
         >
           <img src={snoopyWrites} alt="snoopy-writes-banner" className="w-[15vw] mt-4" />
-          <ToolBar editor={editor}/>
+          <ToolBar editor={editor} />
         </section>
       </section>
     </Slate>
