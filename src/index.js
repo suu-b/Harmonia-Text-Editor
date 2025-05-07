a515c20a67b877b78f7c33b3b6435f29e18bb34b
suu-b <thakur0805@gmail.com>
2025-05-07 11:12:40 +0530

    code all set to publish

---
+++ src/index.js
@@ -0,0 +1,7 @@
+export {default as HarmoniaTextEditor} from './api/Editor.jsx'
+export {createMyEditor} from './api/createEditor'
+
+export * as toggleAlignment from "./api/toggle/toggleAlignment"
+export * as toggleBlocks from "./api/toggle/toggleBlocks"
+export * as toggleLists from "./api/toggle/toggleLists"
+export * as toggleMark from "./api/toggle/toggleMark"
--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,33 @@
 import path from "path"
-import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    lib:{
+      entry: path.resolve(__dirname, 'src/index.js'),
+      name: "HarmoniaTextEditor",
+      fileName:"index",
+      formats: ["es", "cjs"],
+    },
+    rollupOptions:{
+      external: ["react", "react-dom", "slate", "slate-react"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+          slate: "Slate",
+          "slate-react": "SlateReact",
+        },
+      },
+    },
+    outDir: 'dist'
+  }
 })
